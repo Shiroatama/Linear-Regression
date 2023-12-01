71e566ba45a66d5af88ebccd140db64de602df42
Nep <38284741+Re3ses@users.noreply.github.com>
2023-12-01 11:56:48 +0800

    Added new Linear Regression Model
    Used SGDRegressor or Gradient Descent model from SKL.

--- multivariate_linear_regression.py
+++ multivariate_linear_regression.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
+# %matplotlib inline
 
 # %% [markdown]
 # # Load the data #
@@ -23,15 +24,14 @@ df_data.drop('No', inplace=True, axis=1)
 df_data.head()
 df_data.info()
 
-# Declare the dependent variables
+# Declare the dependent variables 
 features = df_data.drop('Y house price of unit area', axis=1).columns
-print("features : ", features)
 
 # %% [markdown]
 # # Plotting the independent and dependent variables #
 
 # %%
-sns.pairplot(df_data, x_vars=features, y_vars='Y house price of unit area')
+sns.pairplot(df_data, x_vars=features, y_vars='Y house price of unit area', aspect=1, height=5)
 
 # %% [markdown]
 # # Plotting the heatmap #
@@ -40,19 +40,26 @@ sns.pairplot(df_data, x_vars=features, y_vars='Y house price of unit area')
 sns.heatmap(df_data.corr(), annot=True)
 
 # %% [markdown]
-# # Using Linear Regression #
+# # Using Linear Regression model #1 #
 
 # %%
 from sklearn.linear_model import LinearRegression
 
-LinearModel = LinearRegression()
 x = df_data.drop('Y house price of unit area', axis=1)
 y = df_data['Y house price of unit area']
+
+LinearModel = LinearRegression()
 LinearModel.fit(x, y)
 
+# Print the intercept for the model
+# This is sort of a 'base guess' for the price of a house
 print(LinearModel.intercept_)
+
+# Print the coefficients for each dependent variable
+# You could say that for each 1 unit increase in a variable, house price increases or decreases according to the coefficient value 
 print(LinearModel.coef_)
 
+# Print the coefficients witht heir labels
 list(zip(features, LinearModel.coef_))
 
 # %% [markdown]
@@ -72,6 +79,61 @@ print("R2 : ", r2_score(y, LinearModel_Predictions))
 
 # %%
 plt.scatter(y, LinearModel_Predictions)
+plt.xlabel("Actual Values")
+plt.ylabel("Predicted Values")
+plt.title("Actual vs Predicted Values for House Price")
+plt.show()
+
+# %% [markdown]
+# # Using Linear Regression model #2 #
+
+# %%
+from sklearn.linear_model import SGDRegressor
+
+SGDModel = SGDRegressor(learning_rate="constant", max_iter=10000, tol=1e-3)
+x = df_data.drop('Y house price of unit area', axis=1)
+y = df_data['Y house price of unit area']
+
+# %% [markdown]
+# ### Scaling the data ###
+
+# %%
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
+
+# %% [markdown]
+# ### Computing for the intercept and coefficients ###
+
+# %%
+SGDModel.fit(x_scaled, y)
+
+# Print the intercept for the model
+# This is sort of a 'base guess' for the price of a house
+print("SGD Regressor Intercept : ", SGDModel.intercept_)
+
+# Print the coefficients for each dependent variable
+# You could say that for each 1 unit increase in a variable, house price increases or decreases according to the coefficient value 
+print("SGD Regressor Coefficient : ", SGDModel.coef_)
+
+# Print the coefficients witht heir labels
+list(zip(features, SGDModel.coef_))
+
+# %% [markdown]
+# ### Calculating the linear model #2 accuracy ###
+
+# %%
+SGD_Predictions = SGDModel.predict(x_scaled)
+
+print("mean_squared_error : ", mean_squared_error(y, SGD_Predictions))  
+print("R2 : ", r2_score(y, SGD_Predictions))
+
+# %% [markdown]
+# ## Plotting the graph for the model #2 ##
+
+# %%
+plt.scatter(y, SGD_Predictions)
 plt.xlabel("Actual Values")
 plt.ylabel("Predicted Values")
 plt.title("Actual vs Predicted Values for House Price")
